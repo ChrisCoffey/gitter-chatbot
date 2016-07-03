4b7dffe20e3131a943f55d8add5833ad0fe88a05
Chris Coffey <cmcof273@gmail.com>
2016-07-03 12:21:29 -0400

    Add License & beginnings of a readme

---
+++ Readme.md
@@ -0,0 +1,10 @@
+EnsimeBot
+==============
+
+EnsimeBot is a Gitter chatbot to help people use Ensime. 
+
+### How it works
+EnsimeBot listens for traffic on various Gitter channels & performs some simple regular expresison matches against the text. Right now, if there is a user mention & a regex match, EnsimeBot responds with the configured message (i.e. the troubleshooting page).
+
+
+
--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -19,10 +19,12 @@ object Rooms {
     lazy val allRooms: Seq[String] = rulesForRoom.keys.toSeq
     
     val rulesForRoom = Map (
+        //Ensime Server
         "54b58225db8155e6700e9eb0" -> List[(String, String)] (
             ".* error.*" -> "http://ensime.github.io/getting_help/",
             ".* troubleshooting.*" -> "http://ensime.github.io/getting_help/"
             ),
+        //Ensime Vim
         "55e0d9070fc9f982beaef2e3" -> List[(String, String)] (
             ".* error.*" -> "http://ensime.github.io/editors/vim/troubleshooting/",
             ".* troubleshooting.*" -> "http://ensime.github.io/getting_help/"
